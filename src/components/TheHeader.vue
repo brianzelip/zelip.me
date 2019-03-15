fed78ecca6a6d8a57b95afcbc4c6996f0f9fc7c1
Brian Zelip <bzelip@gmail.com>
2019-03-15 15:16:54 -0400

    TheHeader.vue: Create component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="sm-mt4 md-mt5 narrow-container">
+    <div class="flex flex-column align-center sm-flex-row sm-justify-center py2 sm-py0">
+      <img
+        class="circle mb1 sm-mb0 sm-mr2 w-200px md-250px"
+        src="img/bz-livingroom0.jpg"
+      >
+      <div class="px1 center sm-left-align">
+        <h1 class="m0 h0 lh-1p2 white">Brian Zelip</h1>
+        <!-- <p class="m0 p0 white h1 lh-1">web &middot; data &middot; teaching</p> -->
+        <nav class="flex flex-wrap justify-around sm-justify-between mt1 sm-mt2">
+          <a
+            class="h2 white sm-px0 sm-m0 green-underline text-decoration-none lh-1"
+            href="mailto:brian.zelip@gmail.com"
+          >
+            <i class="fa fa-envelope-o sm-hide"></i>
+            <span class="none sm-block">Email</span>
+          </a>
+          <a
+            class="h2 white sm-px0 sm-m0 green-underline text-decoration-none lh-1"
+            href="https://twitter.com/bzelip"
+          >
+            <i class="fa fa-twitter sm-hide"></i>
+            <span class="none sm-block">Twitter</span>
+          </a>
+          <a
+            class="h2 white sm-px0 sm-m0 green-underline text-decoration-none lh-1"
+            href="https://github.com/brianzelip"
+          >
+            <i class="fa fa-github sm-hide"></i>
+            <span class="none sm-block">GitHub</span>
+          </a>
+          <a
+            class="h2 white sm-px0 sm-m0 green-underline text-decoration-none lh-1"
+            href="https://www.linkedin.com/in/zelip"
+          >
+            <i class="fa fa-linkedin sm-hide"></i>
+            <span class="none sm-block">LinkedIn</span>
+          </a>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
