8680760258f29f3fc3f3dd11f70c3d04e259b109
Brian Zelip <bzelip@gmail.com>
2019-03-15 15:05:32 -0400

    main.js: Add global stylesheets

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '~/layouts/Default.vue';
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+
+  // Add an external CSS file
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'http://zelip.me/css/zelip.me.min.css'
+  });
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/fontawesome/4.6.3/css/font-awesome.min.css'
+  });
 }
