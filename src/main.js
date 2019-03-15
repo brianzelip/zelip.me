f1c8673af47ac36f13deb31b32bf3cad9e6b00fc
Brian Zelip <bzelip@gmail.com>
2019-03-15 15:15:57 -0400

    main.js: Add classes to body

--- src/main.js
+++ src/main.js
@@ -7,14 +7,18 @@ export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
 
-  // Add an external CSS file
+  // Add external CSS files, one at a time
   head.link.push({
     rel: 'stylesheet',
     href: 'http://zelip.me/css/zelip.me.min.css'
   });
-
   head.link.push({
     rel: 'stylesheet',
     href: 'https://cdn.jsdelivr.net/fontawesome/4.6.3/css/font-awesome.min.css'
   });
+
+  // Add atributes to <body>
+  head.bodyAttrs = {
+    class: 'bg-dusty-blue px3'
+  };
 }
