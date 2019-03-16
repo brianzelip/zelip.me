77e78524849e5a4983e869547d5b28bfb632dc00
Brian Zelip <bzelip@gmail.com>
2019-03-16 15:56:09 -0400

    Index.vue: Add the code I want to write, although it doesnt work yet

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,25 +2,24 @@
   <Layout>
     <main class="sm-mt3 md-mt4 sm-mb4 narrow-container white">
       <div class="flex flex-column">
-        <p
-          class="h2 p1 m0 fw-200"
-          v-html="bio"
-        ></p>
+        <p class="h2 p1 m0 fw-200">{{ bio }}</p>
       </div>
     </main>
   </Layout>
 </template>
 
 <script>
+const MarkdownIt = require("markdown-it");
+
+import Bio from "../content/bio.md";
+
+const md = new MarkdownIt();
+
 export default {
-  data() {
-    return {
-      bio: `Brian Zelip is a husband, father, and librarian. He runs the
-          <a
-            href="http://www.hshsl.umaryland.edu/services/ispace"
-          >HS/HSL Innovation Space</a>, where he collaborates with library users to solve problems through
-          design, 3D printing, and code.`
-    };
+  computed: {
+    bio() {
+      return md.render(Bio);
+    }
   }
 };
 </script>
