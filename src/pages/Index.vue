dec9e018397b92bcd509a5ff80bd38a22be6ca4f
Brian Zelip <bzelip@gmail.com>
2019-03-16 11:56:34 -0400

    Index.vue: Add bio html from zelip.me to template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,26 @@
 <template>
-  <Layout></Layout>
+  <Layout>
+    <main class="sm-mt3 md-mt4 sm-mb4 narrow-container white">
+      <div class="flex flex-column">
+        <p
+          class="h2 p1 m0 fw-200"
+          v-html="bio"
+        ></p>
+      </div>
+    </main>
+  </Layout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      bio: `Brian Zelip is a husband, father, and librarian. He runs the
+          <a
+            href="http://www.hshsl.umaryland.edu/services/ispace"
+          >HS/HSL Innovation Space</a>, where he collaborates with library users to solve problems through
+          design, 3D printing, and code.`
+    };
+  }
+};
+</script>
