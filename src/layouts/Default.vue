23966e76398ddc0a7be31232b73070b3f982616c
Brian Zelip <bzelip@gmail.com>
2019-03-17 06:25:12 -0400

    layouts/Default: Simplify down to slot only

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,46 +1,5 @@
 <template>
   <div>
-    <TheHeader></TheHeader>
     <slot></slot>
   </div>
 </template>
-
-<script>
-import TheHeader from "../components/TheHeader.vue";
-
-export default {
-  components: {
-    TheHeader
-  }
-};
-</script>
-
-
-<style>
-body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
