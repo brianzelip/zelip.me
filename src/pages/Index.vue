91ca97e813934784f2bcbc82a4ebee53ed65ed5b
Brian Zelip <bzelip@gmail.com>
2019-03-17 06:14:45 -0400

    Index.vue: Write out what I think the markdown import process should look like, but does not work

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,23 +2,22 @@
   <Layout>
     <main class="sm-mt3 md-mt4 sm-mb4 narrow-container white">
       <div class="flex flex-column">
-        <p class="h2 p1 m0 fw-200">{{ bio }}</p>
+        <p
+          class="h2 p1 m0 fw-200"
+          v-html="bio"
+        ></p>
       </div>
     </main>
   </Layout>
 </template>
 
 <script>
-const MarkdownIt = require("markdown-it");
-
 import Bio from "../content/bio.md";
-
-const md = new MarkdownIt();
 
 export default {
   computed: {
     bio() {
-      return md.render(Bio);
+      return Bio;
     }
   }
 };
