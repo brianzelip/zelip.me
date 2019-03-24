32731473a75d27d0b9cb6620e4a049fd3155226a
Brian Zelip <bzelip@gmail.com>
2019-03-24 11:13:23 -0400

    App.vue: Create file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Brian Zelip</h1>
+    <div v-html="bio"></div>
+  </div>
+</template>
+
+<script>
+import MarkdownIt from "markdown-it";
+
+import Bio from "./content/bio.md";
+
+export default {
+  computed: {
+    bio() {
+      const md = new MarkdownIt();
+      return md.render(Bio);
+    }
+  }
+};
+</script>
+
