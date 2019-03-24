196d8f0332ea85489cbb80f65aa0623c29fdf46e
Brian Zelip <bzelip@gmail.com>
2019-03-24 19:01:22 -0400

    TheBio.vue: Fix css inheritence bug by changing element p to div

--- src/components/TheBio.vue
+++ src/components/TheBio.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="sm-mt3 md-mt4 sm-mb4 narrow-container white">
     <div class="flex flex-column">
-      <p
+      <div
         class="h2 p1 m0 fw-200"
         v-html="bio"
-      ></p>
+      ></div>
     </div>
   </main>
 </template>
