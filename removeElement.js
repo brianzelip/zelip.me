604cb1b6ea7e76623e4b817fd8928ec090751fb4
Brian Zelip <bzelip@gmail.com>
2019-03-26 10:46:58 -0400

    removeElement.js: Query dist/index.html via jsdom

--- removeElement.js
+++ removeElement.js
@@ -5,5 +5,12 @@ const { JSDOM } = jsdom;
 const file = process.argv[2];
 const html = fs.readFileSync(file).toString(); // buffer.toString()
 
-console.log('file:', file);
-console.log('html', html);
+const dom = new JSDOM(html);
+const document = dom.window.document;
+
+const title = document.querySelector('title').innerHTML;
+
+// console.log(`file:\t\t`, file);
+// console.log(`html:\n`, html);
+// console.log(`dom:\n`, dom);
+console.log(`title:\t`, title);
