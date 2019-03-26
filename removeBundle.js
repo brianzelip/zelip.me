0d57e85d2f3b0f1afe619a20851001a156f81aa5
Brian Zelip <bzelip@gmail.com>
2019-03-26 12:00:23 -0400

    removeBundle: Clean up debuggers

--- removeBundle.js
+++ removeBundle.js
@@ -6,19 +6,11 @@ const file = process.argv[2];
 const html = fs.readFileSync(file).toString(); // buffer.toString()
 
 const dom = new JSDOM(html);
-
 const document = dom.window.document;
-
 const scriptEl = document.body.querySelector('script');
-
-console.log('body PRE body.removeChild()', document.body.innerHTML);
 
 document.documentElement.children[1].removeChild(scriptEl);
 
-console.log('body POST body.removeChild()', document.body.innerHTML);
-
 const unbundledHTML = dom.serialize();
-
-console.log('unbundledHTML::::', unbundledHTML);
 
 fs.writeFileSync(file, unbundledHTML, 'utf8');
