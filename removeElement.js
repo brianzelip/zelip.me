602928e5eb4a4911d5ecb331d0c3612e003ec332
Brian Zelip <bzelip@gmail.com>
2019-03-26 08:12:55 -0400

    removeElement: Add buffer comment

--- removeElement.js
+++ removeElement.js
@@ -3,7 +3,7 @@ const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
 const file = process.argv[2];
-const html = fs.readFileSync(file).toString();
+const html = fs.readFileSync(file).toString(); // buffer.toString()
 
 console.log('file:', file);
 console.log('html', html);
