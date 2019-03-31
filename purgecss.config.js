926df9f0063d4b55b511f1d4eac442e2cc915ec1
Brian Zelip <bzelip@gmail.com>
2019-03-31 15:27:57 -0400

    purgecss.config: Recognize the needed FA styles

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   content: ['index.html', 'src/**/*.vue'],
-  css: ['src/css/main.css']
+  css: ['src/css/main.css'],
+  whitelistPatterns: [/svg-inline--fa/, /fa-w-14$/, /fa-w-16$/],
+  keyframes: true
 };
